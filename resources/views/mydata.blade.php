5953252e7afd279fc3ddf5bd117ff41e218c9dd3
Ushan Alwis Github <104507543+UshanAlwis98@users.noreply.github.com>
2025-07-09 12:05:42 +0530

    Blade loops learned.

--- resources/views/mydata.blade.php
+++ resources/views/mydata.blade.php
@@ -1,6 +1,6 @@
 This is mydata view file.
 <a href="/readit">Navigate to blogs page</a>
-
+<br />
 <?php
 
 for ($i = 5; $i < 10; $i++) {
@@ -8,3 +8,36 @@ for ($i = 5; $i < 10; $i++) {
 }
 
 ?>
+<br />
+@for ($i = 6; $i < 10; $i++)
+    {{ $i }}
+@endfor
+<br />
+
+<?php
+$name = ['John', 'Jane', 'Doe'];
+foreach ($name as $n) {
+    echo $n . ' ';
+}
+?>
+
+<br />
+@php
+$name2 = ['Ushan', 'Loshitha', 'Alwis'];
+@endphp
+@foreach ($name2 as $n2)
+    {{ $n2 }}
+@endforeach
+<br />
+
+@php
+    $mynumber = 1;
+@endphp
+
+@while ($mynumber <= 5)
+    {{ $mynumber }}
+    @php
+        $mynumber++;
+    @endphp
+
+@endwhile
